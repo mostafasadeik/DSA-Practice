180fc4f460e77d54aa9be754bdca6b9fd95276a3
Mostafa Mohamed <mostafa_dev@icloud.com>
2025-04-24 21:01:51 +0200

    first duplicate value

---
+++ arrays/first-duplicate-value.py
@@ -0,0 +1,22 @@
+# Arrays (First Duplicate Value)
+# Given an array of integers, the task is to find the first duplicate value in the array.
+# If there are no duplicates, return -1.
+# The function first_duplicate_value takes an array of integers as input and returns the first duplicate value or -1 if there are no duplicates.
+# The time complexity of this solution is O(n), where n is the length of the array.
+# The space complexity is O(n) in the worst case, where all elements are unique.
+# Approach:
+
+
+def firstDuplicateValue(array):
+    for value in array:
+        absValue = abs(value)
+        
+        if absValue - 1 >= len(array):  
+            continue  
+        if array[absValue - 1] < 0:
+            return absValue
+        array[absValue - 1] *= -1
+    return -1
+
+result = firstDuplicateValue([2, 1, 5, 2, 3, 2])
+print(result)  # Output: 3
