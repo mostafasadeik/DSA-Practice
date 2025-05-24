784d11e49e8cf06834edbdec97cc34729e0873b3
Mostafa Mohamed <mostafa_dev@icloud.com>
2025-05-24 18:00:07 +0300

    Update insertion sort

--- Sorting/insertion-sort.py
+++ Sorting/insertion-sort.py
@@ -1,18 +1,15 @@
 # Sorting (Insertion Sort)
 
 def insertionSort(array):
-    # Traverse through 1 to len(array)
     for i in range(1, len(array)):
-        key = array[i]
-        j = i - 1
-
-        # Move elements of array[0..i-1], that are greater than key,
-        # to one position ahead of their current position
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(j , j - 1, array)
             j -= 1
-        array[j + 1] = key
     return array
+
+def swap(i , j, array):
+    array[i], array[j] = array[j], array[i]
 # Example usage
 if __name__ == "__main__":
     arr = [12, 11, 13, 5, 6]
